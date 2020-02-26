f84d81979094155f16e0a0fc74a3777c93807294
Jefferson <jeffarrubla@gmail.com>
2020-02-26 21:01:54 +0000

    Updating state immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -14,6 +14,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={() => this.props.onAddCounter( 5 )}  />
                 <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter( 5 )}  />
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                {this.props.storedResults.map(strResult => (
+                       <li key={strResult.id} onClick={this.props.onDeleteResult}>{strResult.value}</li>
+                ))}                           
+                </ul>
             </div>
         );
     }
@@ -21,7 +28,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 };
 
@@ -30,7 +38,9 @@ const mapDispatchToProps = dispatch => {
         onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
         onAddCounter: (val) => dispatch({type: 'ADD', value: val}),
-        onSubtractCounter: (val) => dispatch({type: 'SUBTRACT', value: val})
+        onSubtractCounter: (val) => dispatch({type: 'SUBTRACT', value: val}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
     };
 };
 
